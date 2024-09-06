d254c5624e7b7e02a999a408758a498512780c14
Aaron J. Girton <agirton@intradark.com>
2024-09-06 15:52:33 +1000

    feat: add base layout for login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <main>
+      <h1>Login</h1>
+      {/* Placeholder for login form */}
+      <form>{/* Form to be placed here later */}</form>
+    </main>
+  );
+};
+
+export default Login;
