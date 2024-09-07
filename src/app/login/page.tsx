b05aca728896f869e45928f5909c9924d798ba7f
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-09-07 23:34:31 +1000

    feat: create login page with responsive layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import Image from 'next/image';
+import { LoginForm } from '@/components/organisms/LoginForm/LoginForm';
 
 const Login = () => {
   return (
-    <main>
-      <h1>Login</h1>
-      {/* Placeholder for login form */}
-      <form>{/* Form to be placed here later */}</form>
-    </main>
+    <div className='w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]'>
+      <div className='flex items-center justify-center py-12'>
+        <LoginForm />
+      </div>
+      <div className='hidden bg-muted lg:block'>
+        <Image
+          src='/placeholder.svg'
+          alt='Image'
+          width='1920'
+          height='1080'
+          className='h-full w-full object-cover dark:brightness-[0.2] dark:grayscale'
+        />
+      </div>
+    </div>
   );
 };
 
