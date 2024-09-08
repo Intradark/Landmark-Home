4b4006e271499bf09ff6a68cc1a8bd33f23e0287
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-09-09 04:37:49 +1000

    refactor(dashboard): Update dashboard layout

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -51,10 +51,8 @@ export default function Dashboard() {
   ];
 
   return (
-    <div
-      className={`min-h-screen bg-background text-foreground max-w-6xl mx-auto`}
-    >
-      <main className='p-4 space-y-4'>
+    <div className='bg-background text-foreground'>
+      <div className='max-w-6xl mx-auto p-4 space-y-4'>
         <StatsGrid />
         <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-7'>
           <div className='col-span-4'>
@@ -84,7 +82,43 @@ export default function Dashboard() {
             <NewsList news={newsData} />
           </div>
         </div>
-      </main>
+        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-7'>
+          <div className='col-span-3'>
+            <DataTable
+              title='Tasks'
+              headers={tasksData.headers}
+              rows={tasksData.rows}
+            />
+          </div>
+          <div className='col-span-4'>
+            <NewsList news={newsData} />
+          </div>
+        </div>
+        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-7'>
+          <div className='col-span-3'>
+            <DataTable
+              title='Tasks'
+              headers={tasksData.headers}
+              rows={tasksData.rows}
+            />
+          </div>
+          <div className='col-span-4'>
+            <NewsList news={newsData} />
+          </div>
+        </div>
+        <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-7'>
+          <div className='col-span-3'>
+            <DataTable
+              title='Tasks'
+              headers={tasksData.headers}
+              rows={tasksData.rows}
+            />
+          </div>
+          <div className='col-span-4'>
+            <NewsList news={newsData} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
